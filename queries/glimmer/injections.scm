26c8557fc8cc6e80f5cc350be50eb7a78a4ae17c
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-08-19 19:08:53 -0400

    How is this CSS injection going to work?

--- queries/glimmer/injections.scm
+++ queries/glimmer/injections.scm
@@ -1,21 +1,34 @@
 ; <style> tags
-(
-  (element_node
-    (element_node_start (tag_name) @_tag_name)
-    ((text_node) @injection.content
-      (#set! injection.language "css"))
-
+((element_node
+  (element_node_start
+    (tag_name) @_tag_name
+    (#eq? @_tag_name "style")
   )
-  (#eq? @_tag_name "style")
+  )
+  @injection.content
+  (#set! injection.language "css")
+  (#set! injection.include-children)
+  (#set! injection.self)
+  ; (#set! injection.parent)
+  (#offset! @injection.content 0 6 0 -7)
 )
+; ((element_node) @injection.content
+;  (#set! injection.language "css")
+;  (#set! injection.include-children)
+;  (#set! injection.self)
+;  (#set! injection.parent)
+; )
 
-; script tags
+
+; <script> tags
 (
   (element_node
     (element_node_start (tag_name) @_tag_name)
-    ((text_node) @injection.content
-      (#set! injection.language "javascript"))
-
+    ((text_node) @injection.content)
   )
   (#eq? @_tag_name "script")
+  (#set! injection.language "javascript")
+  (#set! injection.include-children)
 )
+
+

0f8ac7dbeaaead9c6f60fe2c7d159da7ec35a799
Bjarne Mogstad <me@mogstad.co>
2025-03-19 15:21:54 +0100

    Native support for else and else if in block statements
    As there isn't a native concept of if statements in the grammar, it
    makes it especially hard to query `else if` statements, as they've
    previously just been exposed as a helper. It makes it cumbersome to
    query `else` blocks and not possible—I think at least—to query and
    capture `else if` as the same capture.
    
    `else` is supported for all block_statements in Ember, but `else if`
    isn't, but it wouldn't be valid syntax that we want to capture any other
    way, so I haven't tried to be cleaver about it.

--- grammar.js
+++ grammar.js
@@ -315,6 +315,23 @@ export default grammar({
         choice("}}", "~}}"),
       ),
 
+    block_statement_inverse: ($) =>
+      choice(
+        seq(
+          choice("{{", "{{~"),
+          "else",
+          optional($.block_params),
+          choice("}}", "~}}"),
+        ),
+        // prettier-ignore
+        seq(
+          choice("{{", "{{~"),
+          "else if",
+          $._arguments,
+          choice("}}", "~}}"),
+        ),
+      ),
+
     block_statement_end: ($) =>
       seq(
         choice("{{/", "{{~/"),
@@ -325,7 +342,9 @@ export default grammar({
     block_statement: ($) =>
       seq(
         $.block_statement_start,
-        field("program", repeat($._declaration)),
+        repeat(
+          choice($.block_statement_inverse, field("program", $._declaration)),
+        ),
         $.block_statement_end,
       ),
   },
